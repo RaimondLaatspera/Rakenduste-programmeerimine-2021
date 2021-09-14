fbcdb1de07acfb0689012802cf7efd75f2eda013
BuildTools <unconfigured@null.spigotmc.org>
2021-09-14 18:42:15 +0300

    Esimene kodutöö peaks olema valmis.

---
+++ Backend2/src/main/java/ee/mihkel/backend2/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package ee.mihkel.backend2.controller;
+import ee.mihkel.backend2.model.Category;
+import ee.mihkel.backend2.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+
+    @GetMapping("categories")
+    public List<Category> getCategories() {
+        return categoryService.getCategories();
+    }
+
+    @PostMapping("categories")
+    public String postCategory(@RequestBody Category category) {
+        categoryService.saveCategory(category);
+        return "Category added successfully " + category.getCategory();
+    }
+}
