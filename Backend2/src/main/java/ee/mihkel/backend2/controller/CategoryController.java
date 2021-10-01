fbaf03d209083a426a533ac1f22df63a42a9a43c
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 20:57:06 +0300

    Teine kodutöö
    Alguses oli probleeme, kuid peaks olema valmis.

--- Backend2/src/main/java/ee/mihkel/backend2/controller/CategoryController.java
+++ Backend2/src/main/java/ee/mihkel/backend2/controller/CategoryController.java
@@ -2,10 +2,7 @@ package ee.mihkel.backend2.controller;
 import ee.mihkel.backend2.model.Category;
 import ee.mihkel.backend2.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import Item from './components/Item.js';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Navbar from './componets/Navbar';
+import AddItem from './pages/AddItem';
+
 
 function App() {
   return (
     <div className="App">
-      <Item></Item>
+      <Navbar/>
+      <Route path='/' exact>
+        <Home />
+      </Route>
+      <Route path='/cart' exact>
+        <Cart />
+      </Route>
+      <Route path='/add-item' exact>
+        <AddItem />
+      </Route>
     </div>
   );
 }
